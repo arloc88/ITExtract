a74857e4920f53adbc14c4de15d7a13d289d0596
arloc88 <riccio1988@gmail.com>
2017-05-18 19:57:15 +0200

    versione con conteggio caratteri estratti

--- itextract/ITExtract.py
+++ itextract/ITExtract.py
@@ -1,31 +1,42 @@
 import re
 import readDocx
+
 '''
 regex2 = r"""
 	(?s)((\sif).*?(then).+?(?=else|\n)|(else).+?(\n))
 	"""
 '''
 regex = r"(?s)((if\s).*?(then).+?(?=else|if)|(else).+?(\n))"
+
 test_str = readDocx.getText('prova.docx')
 
 
+print('Il documento analizzato contiene %d caratteri' % (len(test_str)))
+
+
 matches = re.finditer(regex, test_str, re.IGNORECASE | re.VERBOSE | re.MULTILINE)
+size=0
 
 for matchNum, match in enumerate(matches):
     matchNum = matchNum + 1
 
     print ("\n ********************** Match {matchNum} was found at {start}-{end} **********************: \n {match}".format(matchNum=matchNum, start=match.start(),
-                                                                       end=match.end(), match=match.group()))
-    '''# Scrivi risultati in un file .
-    matches = open("result.txt", "w")
-    matches.write("Risultati estrazione\nLook at it and see\n \n ********************** Match {matchNum} was found at {start}-{end} **********************: \n {match}".format(matchNum=matchNum, start=match.start(),
+
                                                                    end=match.end(), match=match.group()))
-    matches.close()
-'''
+    lengroup = (match.end() - match.start())
+    print("\n Match lungo %d caratteri" % lengroup)
+    size= size+lengroup
+    print("\n In totale sono stati estratti %d caratteri " %size)
+
+
+    '''
     for groupNum in range(0, len(match.groups())):
         groupNum = groupNum + 1
 
-        '''print (" \n  \t ==>info: Group {groupNum} found at {start}-{end}:'{group}'".format(groupNum=groupNum, start=match.start(groupNum),
+        print (" \n  \t ==>info: Group {groupNum} found at {start}-{end}:'{group}'".format(groupNum=groupNum, start=match.start(groupNum),
                                                                          end=match.end(groupNum),
                                                                          group=match.group(groupNum)))
-        '''
+'''
+
+
+
